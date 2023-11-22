520d70f563fc06e2768bec3df5a49abf94e9844c
ahmed khalifa <aboeleefkhalifa@gmail.com>
2023-11-22 21:42:23 +0200

    stable version by ansible roles

--- ec2-.tf
+++ ec2-.tf
@@ -20,31 +20,11 @@ resource "local_file" "ssh_key" {
   }
 }
 
-resource "aws_instance" "master" {
-  count         = 0
-  ami           = data.aws_ami.ubuntu_ami.id
-  instance_type = "t3.medium"
-  # user_data                   = file("${path.module}/kubernetes/user_data.sh") 
-  key_name                    = aws_key_pair.key_pair.key_name
-  associate_public_ip_address = true
-  subnet_id                   = aws_subnet.public[0].id
-  vpc_security_group_ids      = [aws_security_group.grad_proj_sg["ssh"].id, aws_security_group.grad_proj_sg["http_https"].id, aws_security_group.grad_proj_sg["public"].id] #vpc_security_group_ids      = [ aws_security_group.http-only.id, aws_security_group.ssh-only.id ]
-  root_block_device {
-    volume_size = 10
-  }
-  credit_specification {
-    cpu_credits = "standard" #As T3 instances are launched as unlimited by default. T2 instances are launched as standard by default
-  }
-  tags = {
-    "Name" = "master-node"
-  }
-}
-
 
 resource "aws_instance" "master_nodes" {
   count                       = 1
   ami                         = data.aws_ami.ubuntu_ami.id
-  instance_type               = "t2.medium"
+  instance_type               = "t3.medium"
   key_name                    = aws_key_pair.key_pair.key_name
   associate_public_ip_address = true
   subnet_id                   = aws_subnet.public[0].id
@@ -53,10 +33,10 @@ resource "aws_instance" "master_nodes" {
     volume_size = 10
   }
   credit_specification {
-    cpu_credits = "standard" #As T3 instances are launched as unlimited by default. T2 instances are launched as standard by default
+    cpu_credits = "standard"  #As T3 instances are launched as unlimited by default. T2 instances are launched as standard by default
   }
   tags = {
-    "Name" = "master-node"
+    "Name" = "master-node-${count.index}"
   }
 }
 
@@ -82,7 +62,7 @@ resource "aws_instance" "worker_nodes" {
 
 resource "null_resource" "kube_cluster" {
   provisioner "local-exec" {
-    command = "export master_public_ip=${aws_instance.master_nodes[0].public_ip} master_private_ip=${aws_instance.master_nodes[0].private_ip} worker1_public_ip=${aws_instance.worker_nodes[0].public_ip} worker1_private_ip=${aws_instance.worker_nodes[0].private_ip} worker2_public_ip=${aws_instance.worker_nodes[1].public_ip} worker2_private_ip=${aws_instance.worker_nodes[1].private_ip}; envsubst '$master_public_ip,$master_private_ip,$worker1_public_ip,$worker1_private_ip,$worker2_public_ip,$worker2_private_ip' < ./kubernetes-2/master-node-vars > ./kubernetes-2/master-node.yaml; sleep 125; ansible-playbook --inventory ${aws_instance.master_nodes[0].public_ip},${aws_instance.worker_nodes[0].public_ip},${aws_instance.worker_nodes[1].public_ip} --user ubuntu  ./kubernetes-2/master-node.yaml"
+    command = "export master_public_ip=${aws_instance.master_nodes[0].public_ip} master_private_ip=${aws_instance.master_nodes[0].private_ip} worker1_public_ip=${aws_instance.worker_nodes[0].public_ip} worker1_private_ip=${aws_instance.worker_nodes[0].private_ip} worker2_public_ip=${aws_instance.worker_nodes[1].public_ip} worker2_private_ip=${aws_instance.worker_nodes[1].private_ip} ; envsubst '$master_public_ip,$worker1_public_ip,$worker2_public_ip' < ./kubernetes-2/roles/kubernetes-worker/tasks/main-vars > ./kubernetes-2/roles/kubernetes-worker/tasks/main.yml ; envsubst '$master_public_ip,$master_private_ip,$worker1_public_ip,$worker1_private_ip,$worker2_public_ip,$worker2_private_ip' < ./kubernetes-2/master-node-vars > ./kubernetes-2/master-node.yaml; sleep 125; ansible-playbook --inventory ${aws_instance.master_nodes[0].public_ip},${aws_instance.worker_nodes[0].public_ip},${aws_instance.worker_nodes[1].public_ip} --user ubuntu  ./kubernetes-2/master-node.yaml"
   }
   depends_on = [aws_instance.master_nodes[0],aws_instance.worker_nodes[0],aws_instance.worker_nodes[1]]
   lifecycle {
@@ -92,7 +72,7 @@ resource "null_resource" "kube_cluster" {
 
 resource "null_resource" "ssh_config" {
  provisioner "local-exec" {
-   command = "export master_public_ip=${aws_instance.master_nodes[0].public_ip}  worker1_public_ip=${aws_instance.worker_nodes[0].public_ip} worker2_public_ip=${aws_instance.worker_nodes[1].public_ip} ; envsubst '$master_public_ip,$worker1_public_ip,$worker2_public_ip' < ./ssh-config-vars > ./ssh-config ; cp ssh-config ~/.ssh/ssh.config"
+   command = "export master_public_ip=${aws_instance.master_nodes[0].public_ip}  worker1_public_ip=${aws_instance.worker_nodes[0].public_ip} worker2_public_ip=${aws_instance.worker_nodes[1].public_ip} ; envsubst '$master_public_ip,$worker1_public_ip,$worker2_public_ip' < ./ssh-config-vars > ./ssh-config ; cp ssh-config ~/.ssh/config"
  }
  depends_on = [aws_instance.master_nodes[0],aws_instance.worker_nodes[0],aws_instance.worker_nodes[1],aws_instance.worker_nodes[1],null_resource.kube_cluster]
  lifecycle {
